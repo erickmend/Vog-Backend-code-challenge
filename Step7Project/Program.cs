3c0e357c03123d5d0c983882bf9682cd0ef457b4
Erick Guadalupe Mendoza Barron <erickgmbarron@gmail.com>
2021-12-15 13:41:45 -0700

    [Dev] Iterate list without loops

---
+++ Step7Project/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Step7Project
+{
+    internal class Program
+    {
+        public static class QuestionClass
+        {
+            static List<string> NamesList = new List<string>()
+        {
+            "Jimmy",
+            "Jeffrey",
+            "John",
+        };
+
+
+            static void Main(string[] args)
+            {
+                Iterator(NamesList);
+            }
+
+            private static void Iterator(List<string> list)
+            {
+                if (list.Count() == 0)
+                    return;
+
+                Console.WriteLine(list.FirstOrDefault());
+                int first = 0;
+                list.RemoveAt(first);
+                Iterator(list);
+            }
+        }
+    }
+}
